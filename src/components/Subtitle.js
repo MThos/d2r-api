3840f6d2675a882128a8e51a2fb3563b3e9b0687
MThos <MThos@users.noreply.github.com>
2022-09-18 23:52:31 -0400

    Update Subtitle.js

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Subtitle = (props) => {
   return (
-    <div className="subtitle">&#183;{props.title}&#183;</div>
+    <div className="subtitle">{props.title}</div>
   )
 }
 
