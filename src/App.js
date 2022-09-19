d584bedc372611e50fc54a2cba38015be879fd73
MThos <MThos@users.noreply.github.com>
2022-09-18 23:51:27 -0400

    added footer/header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import ReactTooltip from "react-tooltip";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import List from './components/List';
+import Footer from "./components/Footer";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<List />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
       <ReactTooltip />
     </div>
   );
