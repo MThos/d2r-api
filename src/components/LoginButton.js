c3a6b86f930c471f6be9c4d6c1d87172a85b1a21
MThos <MThos@users.noreply.github.com>
2022-09-29 05:11:22 -0400

    Create LoginButton.js

---
+++ src/components/LoginButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button className="auth0-btn" onClick={() => loginWithRedirect()}>
+      Login / Register
+    </button>
+  );
+};
+
+export default LoginButton;
