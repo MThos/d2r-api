97a6d9788f25eae4d3d2f1c23ab821aba435d54d
MThos <MThos@users.noreply.github.com>
2022-09-29 05:10:41 -0400

    added auth0

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import './css/app.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactTooltip from "react-tooltip";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth0 } from "@auth0/auth0-react";
 import List from './components/List';
 import Footer from "./components/Footer";
 import Header from './components/Header';
+import LoopIcon from '@mui/icons-material/Loop';
+import Terms from './components/Terms';
+import Privacy from './components/Privacy';
+import Cookies from './components/Cookies';
 
 function App() {
+  const { isAuthenticated, user, isLoading } = useAuth0();
+  const [apiKey, setAPIKey] = useState();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      GetAPIKey(user);
+    }
+  }, [isAuthenticated, user]);
+
+  const GetAPIKey = (user) => {
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:8000/getapikey',
+      params: { 'user': user }
+    };
+  
+    axios.request(options).then((response) => {
+      //console.log(response.data['api_key']);
+      setAPIKey(response.data['api_key']);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  if (isLoading) {
+    return (
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center', gap:'10px', marginTop:'23%', textAlign:'center'}}>
+        <LoopIcon id="loading-icon" style={{height:'80px', width:'80px'}} />
+        <span style={{fontSize:'1.3rem', fontWeight:'700'}}>LOADING</span>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<List />} />
+          <Route path="/" element={isAuthenticated ? <List api_key={apiKey} /> : ""} />
+          <Route path="/endpoints" element={isAuthenticated ? <List api_key={apiKey} /> : ""} />
+          <Route path="/cookies" element={<Cookies />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </BrowserRouter>
       <Footer />
