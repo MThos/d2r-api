1227fd781f93a951a9f1e7406bee8cb384aee0a9
MThos <MThos@users.noreply.github.com>
2022-09-29 05:17:46 -0400

    added Auth0 and env variables for client id/domain

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+    useRefreshTokens={true}
+    cacheLocation="localstorage"
+    >
+
+      <App />
+      
+    </Auth0Provider>
   </React.StrictMode>
 );
 
