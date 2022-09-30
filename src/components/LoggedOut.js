9c2d2b63ed81b1aadd09e0f5a4828127ebc1c8fc
MThos <MThos@users.noreply.github.com>
2022-09-30 06:19:27 -0400

    redesigned logged out page

--- src/components/LoggedOut.js
+++ src/components/LoggedOut.js
@@ -4,16 +4,16 @@ const LoggedOut = () => {
   return (
     <main className="fade-in">
       <div id="logged-out-info-top">
-        <p>This API contains information about &#8212; Diablo 2 Resurrected</p>
-        <p>Endpoints &#8212; classes, skills, items, runewords, quests and more...</p>
+        <p>This API contains information about &#8212; Diablo 2 Resurrected!</p>
+        <p>Available &#8212; classes, skills, items, runes, quests, etc.</p>
         <br/>
         <p>Signup to receive a free API key &#8212; 20 requests per minute!</p>
       </div>
       <div>
-        <img id="api-example-img" src="images/amazon_example.png" alt="api example" />
+        <p id="logged-out-info-bottom">Amazon API Call Example</p>
       </div>
       <div>
-        <p id="logged-out-info-bottom">^ example of the Amazon endpoint in JSON format ^</p>
+        <img id="api-example-img" src="images/amazon_example.png" alt="api example" />
       </div>
     </main>
   )
