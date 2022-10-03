61bea29cd36198cd2a53d8f185f84932401bfc30
MThos <MThos@users.noreply.github.com>
2022-10-03 07:14:50 -0400

    Update LoggedOut.js

--- src/components/LoggedOut.js
+++ src/components/LoggedOut.js
@@ -2,18 +2,14 @@ import React from "react";
 
 const LoggedOut = () => {
   return (
-    <main className="fade-in">
+    <main id="logged-out" className="fade-in">
       <div id="logged-out-info-top">
-        <p>This API contains information about &#8212; Diablo 2 Resurrected!</p>
-        <p>Available &#8212; classes, skills, items, runes, quests, etc.</p>
-        <br/>
-        <p>Signup to receive a free API key &#8212; 20 requests per minute!</p>
+        <p>This API contains information about &#8212; Diablo 2 Resurrected</p>
+        <p>Available API &#8212; classes, skills, items, runes, quests and more</p>
+        <p>Signup to receive a free API key &#8212; 20 requests per minute</p>
       </div>
-      <div>
-        <p id="logged-out-info-bottom">Amazon API Call Example</p>
-      </div>
-      <div>
-        <img id="api-example-img" src="images/amazon_example.png" alt="api example" />
+      <div id="logged-out-img">
+        <img src="images/amazon_example.png" alt="api example" />
       </div>
     </main>
   )
