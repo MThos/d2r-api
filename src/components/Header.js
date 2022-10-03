651d5b0370e41d78d272e1712ffeb8fa7b18f970
MThos <MThos@users.noreply.github.com>
2022-10-03 07:14:31 -0400

    added 'Not Logged In' to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,17 +14,17 @@ const Header = () => {
         <a href="/"><WebhookIcon id="main-icon" /></a>
       </div>
       <div>
-        <a href="/"><img src="images/d2r_logo_white_name.png" id="header-logo" /></a>
+        <a href="/"><img src="images/d2r_logo_white_name.png" id="header-logo" alt="d2r-api logo" /></a>
       </div>
       <div id="user-login">
-        <div id="user">
-        {
-          isAuthenticated ? user.email : "Not Logged In"
-        }
-        </div>
         <div id="user-btn">
         {
           isAuthenticated ? <LogoutButton /> : <LoginButton />
+        }
+        </div>
+        <div id="user">
+        {
+          isAuthenticated ? user.email : "Not Logged In"
         }
         </div>
       </div>
