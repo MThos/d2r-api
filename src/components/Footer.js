cd1724d11722db03b9c9b96db9c4bf7e6c3e7779
MThos <MThos@users.noreply.github.com>
2023-03-12 22:28:24 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <ul>
         <li>Copyright &#169; {new Date().getFullYear()}</li>
         <li><a href="/privacy" className="nav-link">Privacy Policy</a></li>
-        <li><a href="/terms" className="nav-link">Terms and Conditions</a></li>
+        <li><a href="/terms" className="nav-link">Terms of Service</a></li>
         <li><a href="/cookies" className="nav-link">Cookies</a></li>
         <li><a href="/contact" className="nav-link">Contact</a></li>
       </ul>
